3f6e64db5c67e6f0e458a186af1497770476b98c
cfpaige <cfpaige@gmail.com>
2019-07-12 22:58:53 -0700

    initial logic, add works, devour doesn't

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Pulled Pork Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Avocado Blue Cheese Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Grilled Chicken Tomato Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Paleo Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger with Fries');
+INSERT INTO burgers (burger_name) VALUES ('Grilled Veggie Burger');
